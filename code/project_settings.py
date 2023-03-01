ac7ccfb4cb05c1372564cdc46b68286d22902ed8
Julian Geiger <julian.geiger@epfl.ch>
2023-03-01 09:54:19 +0100

    Now, proper ordering with Mn, then O, then rest.

---
+++ code/project_settings.py
@@ -0,0 +1,5 @@
+
+spinel_dir_iurii = '/home/jgeiger/projects/bat_uv_ml/data/spinel_iurii'
+olivine_dir_iurii = '/home/jgeiger/projects/bat_uv_ml/data/olivine_iurii'
+project_dir = '/home/jgeiger/projects/bat_uv_ml'
+pseudo_dir = '/usr/share/espresso/pseudo/'
