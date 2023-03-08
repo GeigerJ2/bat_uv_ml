42a511e16b12de61da26a5f2a3164992eb8d6c48
Julian Geiger <julian.geiger@epfl.ch>
2023-03-08 14:35:33 +0100

    Some notebook cleanup.

--- code/project_settings.py
+++ code/project_settings.py
@@ -1,5 +1,4 @@
-
-spinel_dir_iurii = '/home/jgeiger/projects/bat_uv_ml/data/spinel_iurii'
-olivine_dir_iurii = '/home/jgeiger/projects/bat_uv_ml/data/olivine_iurii'
-project_dir = '/home/jgeiger/projects/bat_uv_ml'
-pseudo_dir = '/usr/share/espresso/pseudo/'
+spinel_dir_iurii = "/home/jgeiger/projects/bat_uv_ml/data/spinel_iurii"
+olivine_dir_iurii = "/home/jgeiger/projects/bat_uv_ml/data/olivine_iurii"
+project_dir = "/home/jgeiger/projects/bat_uv_ml"
+pseudo_dir = "/usr/share/espresso/pseudo/"
